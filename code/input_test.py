7e5dfa28f61e33b522f3cf2430e92e4e911ff50c
remcobdeg <remcodegrave@gmail.com>
2018-12-05 14:19:58 +0000

    inputtest.py added to test in comming line execution

---
+++ code/input_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  5 14:11:29 2018
+
+@author: remcobenthemdegrave
+"""
+ 
+x = input('Enter your name:')
+print('Hello, ' + x)
